833e66747e9b30bb330b11b86863bbde3b150eb5
Bradley Johnson <bradley@heroku.com>
2020-11-04 09:32:06 -0600

    add player

--- player.go
+++ player.go
@@ -1,6 +1,8 @@
 package main
 
+// TODO: Maybe use an interface here? Seems like we want a lot of the same behavior but also will want to have a 'skill' setting for AI players
 type player struct {
 	hasLead bool
 	name    string
+	human   bool
 }
